dcdccf9d9b3e3d6497313804148a20ff0fc97bae
mohammadamrollahi <mohammadamrollahi3@gmail.comm>
2020-09-30 17:18:46 +0330

    start responsive

--- events.js
+++ events.js
@@ -23,5 +23,10 @@ var swiper = new Swiper('.swiper-container', {
         slidesPerView: 4,
         spaceBetween: 50,
       },
-    }
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   });
+  AOS.init();
